61fc389dc9e293d6c928f2f767fdda216e5617b5
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-09-10 02:17:47 +0530

    new:added the suspense api of the react for the loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
+import TodosList from "./(user)/todos/TodosList";
 
 const Home = () => {
-  return <div>I am Home Page</div>;
+  return (
+    <div className="flex flex-col space-y-10">
+      <Suspense
+        fallback={<p className="text-red-500">Loading the Todos.....</p>}
+      >
+        <div className="flex space-x-2">
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <p className="text-blue-500">Loading the Shopping Trolley....</p>
+        }
+      >
+        <div className="flex space-x-2">
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
 };
 
 export default Home;
